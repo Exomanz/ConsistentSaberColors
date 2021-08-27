7d24494389a637f48f94782571fc34bd11c1dfe5
Exomanz <exomanz@users.noreply.github.com>
2021-08-27 03:46:17 -0500

    Add project files.

---
+++ ConsistentSaberColors/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using IPA;
+using IPALogger = IPA.Logging.Logger;
+using UnityEngine.SceneManagement;
+using System;
+using System.Linq;
+
+namespace ConsistentSaberColors
+{
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin
+    {
+        internal static IPALogger Log { get; private set; }
+        internal static Harmony HarmonyID { get; private set; }
+
+        [Init] public Plugin(IPALogger logger)
+        {
+            Log = logger;
+            SceneManager.activeSceneChanged += ActiveSceneChanged;
+        }
+
+        [OnEnable] public void Enable()
+        {
+            if (HarmonyID is null) HarmonyID = new Harmony("bs.Exomanz.csc");
+            HarmonyID.PatchAll(System.Reflection.Assembly.GetExecutingAssembly());
+            Services.PlayerSaveDataBackupHelper.BackupSaveData();
+        }
+        [OnDisable] public void Disable()
+        {
+            HarmonyID.UnpatchAll(HarmonyID.Id);
+            HarmonyID = null;
+        }
+
+        private void ActiveSceneChanged(Scene arg0, Scene arg1)
+        {
+            if (arg1.name == "MainMenu") MenuSaberColorManager.UpdateColors();
+        }
+    }
+}
---
+++ ConsistentSaberColors/Services/MenuSaberColorManager.cs
@@ -0,0 +1,61 @@
+﻿using IPA.Utilities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ConsistentSaberColors
+{
+    public static class MenuSaberColorManager
+    {
+        internal static Color defaultRight = new Color(0.156863f, 0.556863f, 0.823529f, 1.0f);
+        internal static Color defaultLeft = new Color(0.784314f, 0.078431f, 0.078431f, 1.0f);
+
+        public static void UpdateColors()
+        {
+            PlayerDataModel PlayerDataModel;
+            PlayerData playerData;
+
+            PlayerDataModel = GameObject.Find("PlayerDataModel(Clone)")?.GetComponent<PlayerDataModel>();
+            playerData = PlayerDataModel!.GetField<PlayerData, PlayerDataModel>("_playerData");
+
+            if (playerData is null || PlayerDataModel is null) return;
+
+            // Create some empty Colors
+            Color colA;
+            Color colB;
+
+            // Check if our player is overriding colors, and if so, yoink those values
+            if (!playerData.colorSchemesSettings.overrideDefaultColors)
+            {
+                colA = defaultLeft;
+                colB = defaultRight;
+            }
+            else
+            {
+                var x = playerData.colorSchemesSettings.GetField<Dictionary<string, ColorScheme>, ColorSchemesSettings>("_colorSchemesDict");
+                var key = playerData.colorSchemesSettings.selectedColorSchemeId;
+                var val = x[key];
+
+                colA = val.saberAColor;
+                colB = val.saberBColor;
+            }
+
+            var saberGlows_L = GameObject.Find("ControllerLeft/MenuHandle")!.GetComponentsInChildren<SetSaberGlowColor>();
+            var saberGlows_R = GameObject.Find("ControllerRight/MenuHandle")!.GetComponentsInChildren<SetSaberGlowColor>();
+
+            foreach (var x in saberGlows_L)
+            {
+                var colorScheme = x.GetField<ColorManager, SetSaberGlowColor>("_colorManager").GetField<ColorScheme, ColorManager>("_colorScheme");
+                colorScheme.SetField("_saberAColor", colA);
+                x.SetColors();
+            }
+
+            foreach (var y in saberGlows_R)
+            {
+                var colorScheme = y.GetField<ColorManager, SetSaberGlowColor>("_colorManager").GetField<ColorScheme, ColorManager>("_colorScheme");
+                colorScheme.SetField("_saberBColor", colB);
+                y.SetColors();
+            }
+        }
+    }
+}
