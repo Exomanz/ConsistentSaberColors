0c1075acf9b57fa5a4a34a47ce33db085a27c915
Exomanz <exomanz@users.noreply.github.com>
2021-11-03 12:52:32 -0500

    fix nullref in fpfc and version bump

--- ConsistentSaberColors/Plugin.cs
+++ ConsistentSaberColors/Plugin.cs
@@ -3,6 +3,8 @@ using HarmonyLib;
 using IPA;
 using IPALogger = IPA.Logging.Logger;
 using SiraUtil.Zenject;
+using System;
+using System.Linq;
 using System.Reflection;
 
 namespace ConsistentSaberColors
@@ -16,8 +18,18 @@ namespace ConsistentSaberColors
         [Init] public Plugin(IPALogger logger, Zenjector zenject)
         {
             Log = logger;
+            bool fpfc = false;
+
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Any(x => x.ToLower().Contains("fpfc")))
+            {
+                fpfc = true;
+                Log.Notice("FPFC is enabled. PlayerData backup will still occur, but the main color service will not be enabled.");
+            }
 
             zenject.OnApp<PlayerDataServicesProviderInstaller>().WithParameters(Log);
+
+            if (fpfc) return;
             zenject.On<ColorManagerInstaller>().Register<SaberColorManagerInstaller>();
         }
 
